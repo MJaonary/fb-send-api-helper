845e6e7e82e377597d7b33f0591e106fc576f00a
Rajaonarivony Lantonirina Onitiana Mamisoa <ramamilanto@gmail.com>
2022-06-20 21:56:39 +0300

    Integration of a personnal type of message.

---
+++ src/components/personalizedElements.vue
@@ -0,0 +1,45 @@
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: ['id'],
+    computed: {
+        ...mapGetters({
+            content: 'getContent',
+        }),
+        message() {
+            return this.content.json.find(item => item.id == this.id);
+        },
+    },
+    methods: {
+        pauseChange: function (e) {
+            this.message.data.pause = e.target.value;
+        },
+        midChange: function (e) {
+            this.message.data.mid = e.target.value;
+        },
+
+    }
+}
+</script>
+
+<template>
+    <div class="border border-primary col-12 text-center rounded p-1">
+        <div v-if="message.data.pause">
+            Message Pausing for : (in ms)
+            <input type="number" class="text-center form-control" :value="message.data.pause" @input="pauseChange">
+        </div>
+        <div v-if="message.data.mid">
+            Sending Message ID :
+            <input type="text" class="text-center form-control" :value="message.data.mid" @input="midChange">
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
