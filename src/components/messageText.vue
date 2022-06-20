b92d3869ae6807289d2fa3f7f53184c280880c16
Rajaonarivony Lantonirina Onitiana Mamisoa <ramamilanto@gmail.com>
2022-06-20 21:57:55 +0300

    Fixing simple message text and button display

--- src/components/messageText.vue
+++ src/components/messageText.vue
@@ -30,9 +30,9 @@ export default {
         }
     },
     methods: {
-        updateMessage: function(e) {
-        //   let content = this.content;
-          this.content.json.find(item => item.id == this.id).data.message.text = e.target.value;
+        updateMessage: function (e) {
+            //   let content = this.content;
+            this.content.json.find(item => item.id == this.id).data.message.text = e.target.innerHTML;
         },
         addButton: function () {
             let button = {
@@ -64,7 +64,9 @@ export default {
 
 <template>
     <div class="d-flex flex-column border border-primary rounded col-12 px-1">
-        <input type="text" class="bubble" :class="{ 'with-buttons': (buttons?.length) }" :value="message.data?.message?.text" @input="updateMessage">
+        <div type="text" class="bubble" :class="{ 'with-buttons': (buttons?.length) }" contenteditable @input="updateMessage">
+            {{message.data?.message?.text}}
+        </div>
         <button-vue v-for="button in buttons" :id="button.id" :mid="id">
         </button-vue>
         <div class="btn border m-0 p-0 bg-primary text-white" @click="addButton" :hidden="buttons.length > 2">
@@ -80,23 +82,22 @@ export default {
         </div>
     </div>
     <div class="container-fluid">
-            <div class="d-flex flex-column align-items-center">
-                <quick-reply-vue v-for="quick_reply in quick_replies" :id="quick_reply.id" :mid="id"></quick-reply-vue>
-            </div>
+        <div class="d-flex flex-column align-items-center">
+            <quick-reply-vue v-for="quick_reply in quick_replies" :id="quick_reply.id" :mid="id"></quick-reply-vue>
         </div>
-        <div class="btn border m-0 p-0 bg-primary text-white container-fluid" @click="addQuickReply"
-            :hidden="quick_replies?.length > 12">
-            <div
-                class="d-flex align-items-center justify-content-center btn border bg-primary text-white ">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-plus-circle m-1" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                    <path
-                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                </svg>
-                <div>Quick Reply</div>
-            </div>
+    </div>
+    <div class="btn border m-0 p-0 bg-primary text-white container-fluid" @click="addQuickReply"
+        :hidden="quick_replies?.length > 12">
+        <div class="d-flex align-items-center justify-content-center btn border bg-primary text-white ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-plus-circle m-1" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path
+                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+            <div>Quick Reply</div>
         </div>
+    </div>
 </template>
 
 <style>
@@ -109,7 +110,7 @@ export default {
     padding: 6px 12px;
     box-sizing: border-box;
     clear: left;
-    word-wrap: break-word;
+    overflow-wrap: break-word;
     display: inline-block;
     border-radius: 1em;
     -webkit-appearance: textarea;
