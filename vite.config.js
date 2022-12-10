64facafd9447187acb999dab88f9944b6ba23db1
Rajaonarivony Lantonirina Onitiana Mamisoa <ramamilanto@gmail.com>
2022-12-10 08:42:51 +0300

    Added Automatic https Certificate Generation.

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import { fileURLToPath, URL } from "url";
 // A Simple way to import SVG into the template with vite
 import svgLoader from "vite-svg-loader";
 
+// Automatic https Certificate Generation
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: false },
-  plugins: [vue(), svgLoader()],
+  server: { https: true },
+  plugins: [vue(), svgLoader(), basicSsl()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
